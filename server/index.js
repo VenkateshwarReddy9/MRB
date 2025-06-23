8d5ca6845edad2087619b1ecdc59244ba850b8ec
Venkateshwar Reddy Kasturi <kasturivenku@gmail.com>
2025-06-22 23:22:13 -0500

    secrets added

--- server/index.js
+++ server/index.js
@@ -20,13 +20,15 @@ const {
 
 const db = require('./database.js');
 
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
-  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
-  : require('./serviceAccountKey.json');
-
+const serviceAccount = {
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+};
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
 
 db.createTables();
 
