4e1d161da161fd4e0f9cdcc66eefa950dba4452c
Venkateshwar Reddy Kasturi <kasturivenku@gmail.com>
2025-06-22 23:37:44 -0500

    secrets edited1

--- server/index.js
+++ server/index.js
@@ -20,14 +20,18 @@ const {
 
 const db = require('./database.js');
 
-const serviceAccount = {
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+const admin = require('firebase-admin');
+
+const firebaseCert = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
 };
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(firebaseCert)
 });
+
 
 
 db.createTables();
